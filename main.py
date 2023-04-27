eacde2c57017bc0b68c48f02339c48c4a8372f2d
Artem <kozelstudy@gmail.com>
2023-04-27 14:24:54 +0200

    Change

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 import random
-b = input("Введите выбор К-камень, Н-ножници, Б-бумага")
-a = random.choice("КНБ")
-print(a)
-print("Это ваш результат")
+playerchoice = input("Введите выбор К-камень, Н-ножници, Б-бумага:")
+botchice = random.choice("КНБ")
