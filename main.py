fdde9255b8f223463c28852aec43f3cad94accc8
Artem <kozelstudy@gmail.com>
2023-04-27 14:31:38 +0200

    Change

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import random
 playerchoice = input("Введите выбор К-камень, Н-ножници, Б-бумага:")
 botchice = random.choice("КНБ")
+if playerchoice == "К" and botchice == "Н":
+    print("Победа!!!")
+elif playerchoice == "Н" and botchice == "Б":
+    print("Победа!!!")
+elif playerchoice == "Б" and botchice == "К":
+    print("Победа!!!")
+else:
+    print("Бот победил")
