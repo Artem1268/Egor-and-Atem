f20ea880383e8da61a080d2a82d348e8e4603315
Artem <kozelstudy@gmail.com>
2023-04-27 14:40:15 +0200

    Change

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import random
-playerchoice = input("Введите выбор К-камень, Н-ножници, Б-бумага:")
-botchice = random.choice("КНБ")
-if playerchoice == "К" and botchice == "Н":
-    print("Победа!!!")
-elif playerchoice == "Н" and botchice == "Б":
-    print("Победа!!!")
-elif playerchoice == "Б" and botchice == "К":
-    print("Победа!!!")
-else:
-    print("Бот победил")
+playerscore = 0
+botscore = 0
+while True:
+    playerchoice = input("Введите выбор К-камень, Н-ножници, Б-бумага:")
+    botchice = random.choice("КНБ")
+    if playerchoice == "К" and botchice == "Н":
+        print("Победа!!!")
+        playerscore = playerscore + 1
+    elif playerchoice == "Н" and botchice == "Б":
+        print("Победа!!!")
+        playerscore = playerscore + 1
+    elif playerchoice == "Б" and botchice == "К":
+        print("Победа!!!")
+        playerscore = playerscore + 1
+    else:
+        print("Бот победил")
+        botscore = botscore + 1
+        print(f"Твой счёт:{playerscore}, Счёт бота:{botscore}")
