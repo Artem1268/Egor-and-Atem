693ea9c31fe0416501e49d711fc7b89c7f0b2dba
Artem <kozelstudy@gmail.com>
2023-04-30 20:39:41 +0200

    Change

--- password generator.py
+++ password generator.py
@@ -2,3 +2,4 @@ import random
 a = "abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789-_+=!?,."
 length = int(input("Скалько символов должен содержать пароль?:  "))
 password = "".join(random.sample(a, length))
+print(password)
